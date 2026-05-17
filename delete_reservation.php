7c4e064b58547b92ce54283b7198adfe5f60b67e
maram884 <160533890+maram884@users.noreply.github.com>
2026-05-18 01:52:43 +0200

    Add comments to delete_reservation.php for clarity

--- delete_reservation.php
+++ delete_reservation.php
@@ -1,10 +1,13 @@
+
 <?php
-require_once("config.php");
+//supprimer une réservation dans une base de données MySQL.
+require_once("config.php"); //Connexion à la base
 
-$id = $_GET['id'];
+$id = $_GET['id']; //Récupération de l’ID
 
+//Suppression dans la base de données
 mysqli_query($conn,
 "DELETE FROM reservations WHERE id=$id");
 
-header("Location: manage_reservations.php");
+header("Location: manage_reservations.php"); //Après suppression, on redirige vers la page de gestion des réservations.
 ?>
